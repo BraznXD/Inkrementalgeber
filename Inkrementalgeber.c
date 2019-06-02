4ce51c82a6e065feca3256a5faea12e7cec836d8
BraznXD <matthias.breitseher@gmail.com>
2019-06-02 13:02:27 +0200

    Projekte zusammengefasst

--- Inkrementalgeber.c
+++ Inkrementalgeber.c
@@ -1,35 +1,65 @@
 /******************************************************************************/
 /*   (C) Copyright HTL - HOLLABRUNN  2009-2009 All rights reserved. AUSTRIA   */ 
 /*                                                                            */ 
-/* File Name:   External_Interrupt.c                                          */
-/* Autor: 		Josef Reisinger                                               */
+/* File Name:   Inkrementalgeber.c                                         */
+/* Autor: 		Matthias Breitseher / Patrick Steiner                                               */
 /* Version: 	V1.00                                                         */
-/* Date: 		11/03/2012                                                    */
-/* Description: Demoprogramm für External Interrupt                           */
+/* Date: 		02/06/2019                                                    */
+/* Description: Uhr und Positionsanzeige für Inkrementalgeber                                      */
 /******************************************************************************/
 /* History: 	V1.00  creation										          */
 /******************************************************************************/
 #include <armv10_std.h>						 
 
 /*------------------------------ Function Prototypes -------------------------*/
-static void NVIC_init(char position, char priority); 
+static void NVIC_init(char position, char priority);
+static void GPIOA_init(void);
+static void TIM4_Config(void);
 static void EXTI0_Config(void);
 static void EXTI1_Config(void);
+static void set_clock_32MHz(void);
+static void Uhr(void);
+static void position_up(void);
 
 /*------------------------------ Static Variables-------------------------*/
-int aufenthalt;
 volatile int links;
 volatile int rechts;
 volatile int ackr;
 volatile int ackl;
+volatile int zehntel;
+volatile int position;
 
-/******************************************************************************/
-/*           Timer4 Interrupt Service Routine  							                  */
-/******************************************************************************/
-void TIM4_IRQHandler(void)
+static void set_clock_32MHz(void)
 {
+	FLASH->ACR = 0x12;
 	
+	RCC->CR |= RCC_CR_HSEON;
+	while ((RCC->CR & RCC_CR_HSERDY) == 0);
+	
+	RCC->CFGR |= RCC_CFGR_PLLMULL4;
+	RCC->CFGR |= RCC_CFGR_ADCPRE;
+	RCC->CFGR |= RCC_CFGR_PPRE1;
+	RCC->CFGR |= RCC_CFGR_PLLSRC;
+	
+	RCC->CR |= RCC_CR_PLLON;
+	while ((RCC->CR & RCC_CR_PLLRDY) == 0);
+	
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
+	while ((RCC->CFGR & RCC_CFGR_SWS_PLL) == 0);
+	
+	while ((RCC->CFGR & RCC_CFGR_SWS) != ((RCC->CFGR<<2) & RCC_CFGR_SWS));
+	
+	RCC->BDCR |=RCC_BDCR_LSEON;
 }
+/******************************************************************************/
+/*           Interrupt Service Routine  Timer4 (General Purpose Timer)        */
+/******************************************************************************/
+void TIM4_IRQHandler(void)	//Timer 4, löst alle 1000ms aus
+{
+	TIM4->SR &=~0x01;	 //Interrupt pending bit löschen (Verhinderung eines nochmaligen Interrupt-auslösens)
+	zehntel++;
+}
+
 /******************************************************************************/
 /*           External Interrupt Service Routine  Schalter1                    */
 /******************************************************************************/
@@ -50,7 +80,7 @@ void EXTI1_IRQHandler(void)//ISR
 }
 
 /******************************************************************************/
-/*           External Interrupt Service Routine  Schalter1                    */
+/*           External Interrupt Service Routine  Schalter0                    */
 /******************************************************************************/
 void EXTI0_IRQHandler(void)//ISR
 {
@@ -69,9 +99,27 @@ void EXTI0_IRQHandler(void)//ISR
 }
 
 /******************************************************************************/
-/*           Initialization Timer4 (General Purpose Timer)                    */
+/*                     Initialization Timer4 (General Purpose Timer)          */
 /******************************************************************************/
-//static void TIM4_config(void) {/Skriptum S 43
+static void TIM4_Config(void)
+{	
+	/*---------------------- Timer 4 konfigurieren -----------------------*/
+	RCC->APB1ENR |= 0x0004;	//Timer 4 Clock enable
+	TIM4->SMCR = 0x0000;	//Timer 4 Clock Selection: CK_INT wird verwendet
+	TIM4->CR1  = 0x0000;	// Auswahl des Timer Modus: Upcounter --> Zählt: von 0 bis zum Wert des Autoreload-Registers
+
+	/* T_INT = 126,26ns, Annahme: Presc = 150 ---> Auto Reload Wert = 52801 (=0xCE41) --> 1s Update Event*/
+	TIM4->PSC = 800;		//Wert des prescalers (Taktverminderung)
+	TIM4->ARR = 0x400;		//Auto-Reload Wert = Maximaler Zaehlerstand des Upcounters
+	TIM4->RCR = 0;			//Repetition Counter deaktivieren
+
+	/*--------------------- Interrupt Timer 4 konfigurieren ---------------------*/
+	TIM4->DIER = 0x01;	   //enable Interrupt bei einem UEV (Überlauf / Unterlauf)
+	NVIC_init(TIM4_IRQn,1);	   //Enable Timer 4 Update Interrupt, Priority 1
+
+	/*-------------------------- Timer 4 Starten -------------------------------*/
+    TIM4->CR1 |= 0x0001;   //Counter-Enable bit setzen
+}
 
 /******************************************************************************/
 /*                                   EXTI1_config                             */ 
@@ -123,54 +171,106 @@ static void NVIC_init(char position, char priority)
 	NVIC->IP[position]=(priority<<4);	//Interrupt priority register: Setzen der Interrupt Priorität
 	NVIC->ICPR[position >> 0x05] |= (0x01 << (position & 0x1F));//Interrupt Clear Pendig Register: Verhindert, dass der Interrupt auslöst sobald er enabled wird 
 	NVIC->ISER[position >> 0x05] |= (0x01 << (position & 0x1F));//Interrupt Set Enable Register: Enable interrupt
-} 
+}
+
+static void GPIOA_init(void)
+{
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;     	// enable clock for GPIOA (APB2 Peripheral clock enable register)
+	GPIOA->CRL &= 0xFFFFFF00;                  // set Port Pins PA1 to Pull Up/Down Input mode (50MHz) = Mode 8
+	GPIOA->CRL |= 0x00000088;                  
+	GPIOA->ODR |= 0x0002;   					//  Output Register für PA1 auf "1" initialisieren
+	GPIOA->ODR |= 0x0001;
+}
+
+static void Uhr(void)
+{
+	int lcd_zehntel; 		// aktueller Zählerstand auf LCD
+	int sekunden;
+	int lcd_sekunden;
+	int minuten;
+	int lcd_minuten;
+	int stunden;
+	int lcd_stunden;
+	char buffer[30];
+	
+	lcd_zehntel=zehntel%10;
+	sekunden=zehntel/10;
+	lcd_sekunden=sekunden%60;
+	minuten=sekunden/60;
+	lcd_minuten=minuten%60;
+	stunden=minuten/60;
+	lcd_stunden=stunden%60;
+	
+	lcd_set_cursor(0,0);                 // Cursor auf Ursprung
+	sprintf(&buffer[0],"%d:%d:%d:%d", lcd_stunden, lcd_minuten, lcd_sekunden, lcd_zehntel); // zehntelzaehler auf LCD aktualisieren
+  lcd_put_string(&buffer[0]);
+	
+	if(lcd_stunden == 23 & lcd_minuten == 59 & lcd_sekunden == 59 /*& lcd_zehntel == 9*/)	//nur als Testzwecke auskommentiert!!!!
+	{
+		zehntel=0;
+	}
+}
+
+static void position_up(void)
+{
+	char buffer[10];	
+	lcd_set_cursor(1,10);
+	lcd_put_string("       ");
+	lcd_set_cursor(1,10);
+	sprintf(&buffer[0],"%d", position); // zaehler auf LCD aktualisieren
+  lcd_put_string(&buffer[0]);
+	sprintf(&buffer[0],"Position: %d", position);
+	uart_put_string(&buffer[0]);
+
+	
+}
 
 /******************************************************************************/
 /*                                MAIN function                               */
 /******************************************************************************/
 int main (void) 
 {
-int lcd_aufenthalt;
-char buffer[30];
-
-	/* GPIO Init: Schalter auf PA1 Leitung als Eingang initialisieren:	*/		
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;     	// enable clock for GPIOA (APB2 Peripheral clock enable register)
-	GPIOA->CRL &= 0xFFFFFF00;                  // set Port Pins PA1 to Pull Up/Down Input mode (50MHz) = Mode 8
-	GPIOA->CRL |= 0x00000088;                  
-	GPIOA->ODR |= 0x0002;   					//  Output Register für PA1 auf "1" initialisieren
-	GPIOA->ODR |= 0x0001;
-
-	aufenthalt=0;     // Zaehler für falling edges initialisieren
-	lcd_aufenthalt=0; 
-
-    lcd_init ();	     // Initialisieren der LCD Anzeige
-	lcd_clear();		 // LCD Anzeige löschen
-	sprintf(&buffer[0],"Position: %d", lcd_aufenthalt); // zaehler auf LCD aktualisieren
-    lcd_put_string(&buffer[0]);
-
+	int zehntel_pruf=0;
+	int position_pruf=0;
+	
+	set_clock_32MHz();
+	GPIOA_init();
+  uart_init(9600);    // 9600,8,n,1
+  uart_clear();       // Send Clear String to VT 100 Terminal
+  lcd_init();         // LCD initialisieren
+	lcd_clear();        // Lösche LCD Anzeige		
+  zehntel=0; 		// zehntelzaehler initialisieren
+	TIM4_Config();	    // Timer 1 starten: Upcounter --> löst alle 1s einen Update Interrupt
 	EXTI1_Config();		 // Konfigurieren des Externen Interrupts für Leitung PA1 (Falling Edges)
 	EXTI0_Config();
+	uart_put_string("Inkrementalgeber:\n");
+	lcd_set_cursor(1,0);
+	lcd_put_string("Position: 0");
 	do
   {
-		if (lcd_aufenthalt != aufenthalt)    // Anzeigewert geaendert?
+		if (zehntel_pruf != zehntel)    // Hat sich Zählerstand verändert?
+	 	{
+			Uhr();
+			zehntel_pruf=zehntel;	
+		}
+		if (position_pruf != position)
 		{
-			lcd_aufenthalt = aufenthalt;
-			lcd_clear();
-			sprintf(&buffer[0],"Position: %d", lcd_aufenthalt); // zaehler auf LCD aktualisieren
-    	lcd_put_string(&buffer[0]);
+			position_up();
+			position_pruf=position;
 		}
 		if(links==1)
 		{
-			aufenthalt++;
+			position++;
 		}
 		else if(rechts==1)
 		{
-			aufenthalt--;
+			position--;
 		}
 		links=0;
 		rechts=0;
 		ackl=0;
 		ackr=0;
+		
   	} while (1);
 
 }
